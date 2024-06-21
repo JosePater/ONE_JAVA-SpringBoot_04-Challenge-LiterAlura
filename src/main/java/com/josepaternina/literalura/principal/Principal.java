470b45a635ac8751a0748059f7442afc0af3c26c
PaterDev <josepater_1130@hotmail.com>
2024-06-21 07:38:46 -0500

    feat(API): added request and response to book API

--- src/main/java/com/josepaternina/literalura/principal/Principal.java
+++ src/main/java/com/josepaternina/literalura/principal/Principal.java
@@ -1,8 +1,14 @@
 package com.josepaternina.literalura.principal;
 
+import com.josepaternina.literalura.service.ConsumoAPI;
+
 public class Principal {
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private final String URL_BASE = "http://gutendex.com/books/";
 
     public void mostrarMenu() {
         System.out.println("Iniciando proyecto!!!");
+        var json = consumoApi.obtenerDatos(URL_BASE);
+        System.out.println("Los resultados de la consulta: " + json);
     }
 }
